874b608a3aa4baad37a9307699d8ed3f2850506f
Egor <Shevenkov.egor@gmail.com>
2025-07-17 16:07:11 +0300

    В списке картриджей добавлен поиск по всем колонкам.

--- cartridge_accounting/cartridges/views.py
+++ cartridge_accounting/cartridges/views.py
@@ -1,12 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views import View
-from django.views.generic import (
-    ListView, UpdateView, DeleteView)
-from django.utils import timezone
-from .forms import (
-    CartridgeCreateForm,
-    CartridgeUpdateForm,
-)
+from django.views.generic import ListView, UpdateView, DeleteView
+from .forms import CartridgeCreateForm,CartridgeUpdateForm
 from .models import Cartridge, CommissionedCartridge
 from datetime import date
 
@@ -124,3 +121,45 @@ def commissioned_list(request):
     return render(request, 'cartridges/commissioned_list.html', {
         'commissioned_cartridges': commissioned_cartridges
     })
+
+
+class Search(ListView):
+    model = Cartridge
+    template_name = 'cartridges/main.html'
+    context_object_name = 'cartridges'
+
+    def get_queryset(self):
+        name_query = self.request.GET.get('name', '')
+        device_query = self.request.GET.get('device', '')
+        date_from = self.request.GET.get('date_from')
+        date_to = self.request.GET.get('date_to')
+        inv_number_query = self.request.GET.get('inventory_number','')
+        user_query = self.request.GET.get('added_by', '')
+        user_id = self.request.GET.get('user')
+
+        queryset = Cartridge.objects.all()
+
+        if name_query:
+            queryset = queryset.filter(name__icontains=name_query)
+        if device_query:
+            queryset = queryset.filter(device_name__icontains=device_query)
+        if date_from:
+            queryset = queryset.filter(acceptance_date__gte=date_from)
+        if date_to:
+            queryset = queryset.filter(acceptance_date__lte=date_to)
+        if inv_number_query:
+            queryset = queryset.filter(inventory_number__icontains=inv_number_query)
+        if user_query:
+            queryset = queryset.filter(added_by__icontains=inv_number_query)
+        if user_id:
+            queryset = queryset.filter(added_by_id=user_id)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        User = get_user_model()
+        context['name_query'] = self.request.GET.get('name', '')
+        context['device_query'] = self.request.GET.get('device', '')
+        context['users'] = User.objects.all()
+        context['selected_user'] = self.request.GET.get('user', '')
+        return context
